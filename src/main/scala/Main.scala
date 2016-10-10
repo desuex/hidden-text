5ef9f35d2f7ba250f36f17e93a2d53d22a47d236
desuex <nnm043@gmail.com>
2016-10-10 12:48:50 +0300

    rename

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,15 @@
 object Main extends App {
   val v = Array("\u2060", "\u200B", "\u2061", "\u2062", "\u2063", "\uFEFF", "\u200C", "\u200D")
 
-  def valueOf(buf: Array[Byte]): String = buf.map("%03o" format _).mkString
+  def str2oct(buf: Array[Byte]): String = buf.map("%03o" format _).mkString
   def oct2str(string: String): String = string.sliding(3,3).toArray.map(x => BigInt(x,8).toByte.toChar).mkString
 
   def voidEnc(char: String):String = v(char.toInt)
   def voidDec(char:String):String = v.indexOf(char).toString
 
-  def char2void(string: String): String = valueOf(string.getBytes()).map(x=>voidEnc(x.toString)).mkString
+  def char2void(string: String): String = str2oct(string.getBytes()).map(x=>voidEnc(x.toString)).mkString
   def void2char(string: String):String = oct2str(string.map(x=>voidDec(x.toString)).mkString)
-
+  println(str2oct("hello".getBytes()))
   val void = char2void("hello")
   println(void)
   val text = void2char(void)
